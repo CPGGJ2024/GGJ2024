246b02c324c34cec8be9a4fc81dc3c307c19a2fc
Ian-Stentz <130184655+Ian-Stentz@users.noreply.github.com>
2024-01-27 15:11:03 -0800

    Created a Prototype for Infinite Stairs Script

--- Assets/Scripts/SpawnInfiniteStairs.cs
+++ Assets/Scripts/SpawnInfiniteStairs.cs
@@ -1,22 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnInfiniteStairs : MonoBehaviour
 {
     public Transform player;
-    public static GameObject stair;
+    public GameObject stair;
+    public Vector3 initialSpawnLoc;
+    public float stairWidthOffset;
+    public float stairHeightOffset;
+    public int initialStairs = 10;
+    public int maxSpawnedStairs = 60;
 
+    private Vector3 curSpawnLoc;
+    private Queue<GameObject> stairQueue = new Queue<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(stair, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        curSpawnLoc = initialSpawnLoc;
+        for (int i = 0; i < initialStairs; i++)
+        {
+            GameObject newStair = Instantiate(stair, curSpawnLoc, Quaternion.identity);
+            stairQueue.Enqueue(newStair);
+            curSpawnLoc += new Vector3(stairWidthOffset, stairHeightOffset * -1, 0f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while(MathF.Abs(player.position.y - curSpawnLoc.y) < 20)
+        {
+            GameObject newStair = Instantiate(stair, curSpawnLoc, Quaternion.identity);
+            stairQueue.Enqueue(newStair);
+            curSpawnLoc += new Vector3(stairWidthOffset, stairHeightOffset * -1, 0f);
+        }
+        while(stairQueue.Count > maxSpawnedStairs)
+        {
+            Destroy(stairQueue.Dequeue());
+        }
     }
 }
