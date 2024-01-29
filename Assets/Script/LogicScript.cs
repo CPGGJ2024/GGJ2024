fc2784792dd56c7e55323b231668bfc7f6d4fb3b
Kyle Quinlan <35123887+KyleQ1@users.noreply.github.com>
2024-01-28 18:28:43 -0800

    Fix

--- Assets/Script/LogicScript.cs
+++ Assets/Script/LogicScript.cs
@@ -7,12 +7,11 @@ public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
-    public int targetScore = 60;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore = playerScore + 1;
-        scoreText.text = playerScore.ToString() + "/" + targetScore.ToString();
+        scoreText.text = playerScore.ToString();
     }
 }
